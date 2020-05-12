146402e3c7021cae1742f42120933ee96201e067
liuzhizheng <449217551@qq.com>
2020-05-12 09:31:54 +0800

    init

---
+++ src/main/java/com/gx/test/DemoIgnore.java
@@ -0,0 +1,12 @@
+package com.gx.test;
+
+/**
+ * @author zzl
+ * @Date 2020/5/12 9:26
+ */
+public class DemoIgnore {
+
+    public static void main(String[] args) {
+        System.out.println("这是忽略的文件");
+    }
+}
